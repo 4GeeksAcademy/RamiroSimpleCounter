c5feab32389c77e92c6b2d9e95f19138550ad3f4
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-06 20:36:09 +0000

    trabajando con bootstrap

--- src/js/component/secondsCounter.js
+++ src/js/component/secondsCounter.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Contador (promps) {
     return(
-        <div className="text-center">
-            <i className="far fa-clock"></i>
-			<span>{promps.numero6}</span>
-			<span>{promps.numero5}</span>
-			<span>{promps.numero4}</span>
-			<span>{promps.numero3}</span>
-			<span>{promps.numero2}</span>
-			<span>{promps.numero1}</span>			
+        <div className="text-center bg-black">
+            <i className="far fa-clock text-white p-3"></i>
+			<span className="text-white p-3">{promps.numero6}</span>
+			<span className="text-white p-3">{promps.numero5}</span>
+			<span className="text-white p-3">{promps.numero4}</span>
+			<span className="text-white p-3">{promps.numero3}</span>
+			<span className="text-white p-3">{promps.numero2}</span>
+			<span className="text-white p-3">{promps.numero1}</span>			
 		</div>
     )
 }
