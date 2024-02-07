1b3ea0197161eb2360913742fb43c77351bca79e
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-07 09:30:42 +0000

    Cracion final sin bonus

--- src/js/component/secondsCounter.js
+++ src/js/component/secondsCounter.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Contador (promps) {
     return(
-        <div className="text-center bg-black">
-            <i className="far fa-clock text-white p-3"></i>
-			<span className="text-white p-3">{promps.numero6}</span>
-			<span className="text-white p-3">{promps.numero5}</span>
-			<span className="text-white p-3">{promps.numero4}</span>
-			<span className="text-white p-3">{promps.numero3}</span>
-			<span className="text-white p-3">{promps.numero2}</span>
-			<span className="text-white p-3">{promps.numero1}</span>			
+        <div className="text-center bg-black m-5 ">
+            <i className="far fa-clock text-white p-3 bg-dark mx-2 my-5 rounded-4" style={{fontSize: '100px'}}></i>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero6}</span>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero5}</span>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero4}</span>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero3}</span>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero2}</span>
+			<span className="text-white p-3 bg-dark m-2 rounded-4" style={{fontSize: '100px'}}>{promps.numero1}</span>			
 		</div>
     )
 }
